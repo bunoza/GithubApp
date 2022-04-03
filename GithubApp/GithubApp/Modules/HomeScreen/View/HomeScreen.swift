0826858a10141254e5164baa8c74be10b7d6ffd1
bunoza <domagoj.bunoza@gmail.com>
2022-04-03 02:05:53 +0200

    fix naming

---
+++ GithubApp/GithubApp/Modules/HomeScreen/View/HomeScreen.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  GithubApp
+//
+//  Created by Domagoj Bunoza on 28.03.2022..
+//
+
+import SwiftUI
+
+struct HomeScreen: View, HomeScreenDelegate {
+    
+    @ObservedObject var viewModel: HomeScreenViewModel
+    
+    @State private var showingSheet = false
+    @State var text: String = ""
+    
+    init(viewModel: HomeScreenViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        NavigationView {
+            renderContentView()
+                .navigationBarHidden(false)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        renderFilterButton()
+                    }
+                })
+        }
+        .sheet(isPresented: $showingSheet) {
+            SheetView(delegate: self)
+        }
+    }
+    
+    func renderContentView() -> some View {
+        GeometryReader { geometry in
+            VStack {
+                renderLogoImage(geometry: geometry)
+                renderSearchBar(geometry: geometry)
+                renderSearchButton(geometry: geometry)
+            }
+        }
+    }
+    
+    func renderFilterButton() -> some View {
+        Button(
+            action: {
+                showingSheet.toggle()
+            },
+            label: {
+                Image(systemName: "slider.vertical.3")
+                    .resizable()
+                    .foregroundColor(.black)
+            }
+        )
+    }
+    
+    func renderLogoImage(geometry: GeometryProxy) -> some View {
+        Image("github-icon")
+            .resizable()
+            .scaledToFit()
+            .padding(geometry.size.height/7)
+    }
+    
+    func renderSearchBar(geometry: GeometryProxy) -> some View {
+        SearchBar(text: $text, geometry: geometry)
+            .padding(.horizontal, geometry.size.width/7)
+            .padding(.bottom)
+    }
+    
+    func renderSearchButton(geometry: GeometryProxy) -> some View {
+        NavigationLink(
+            destination: {
+                CollectionScreen(
+                    viewModel: CollectionScreenViewModel(
+                        usersSelected: viewModel.usersSelected,
+                        repositoriesSelected: viewModel.repositoriesSelected, githubRepository: GithubRepositoryImpl(), searchQuery: text
+                    )
+                )
+            },
+            label: {
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: geometry.size.width/3.5, height: geometry.size.height/20)
+                    .foregroundColor(.black)
+                    .overlay(content: {
+                        Text("SEARCH")
+                            .foregroundColor(Color.white)
+                            .padding(.horizontal)
+                    })
+            }
+        )
+    }
+    
+    //MARK: - DELEGATE IMPL
+    func toggleUsers() {
+        viewModel.didToggleUsers()
+    }
+    
+    func toggleRepositories() {
+        viewModel.didToggleRepositories()
+    }
+}
+
+//MARK: - DELEGATE
+protocol HomeScreenDelegate {
+    func toggleRepositories()
+    func toggleUsers()
+}
