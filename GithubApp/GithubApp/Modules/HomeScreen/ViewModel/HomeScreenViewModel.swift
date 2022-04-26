6661564fb305ca2f7fbe62b6c80d6650ba793adf
bunoza <domagoj.bunoza@gmail.com>
2022-04-26 15:05:22 +0200

    replace try with switch result, remove onAppear, remove delegate implementation

--- GithubApp/GithubApp/Modules/HomeScreen/ViewModel/HomeScreenViewModel.swift
+++ GithubApp/GithubApp/Modules/HomeScreen/ViewModel/HomeScreenViewModel.swift
@@ -8,69 +8,34 @@
 import Foundation
 import SwiftUI
 
-class HomeScreenViewModel: ObservableObject, HomeScreenViewModelDelegate {
+class HomeScreenViewModel: ObservableObject {
     
     @Published var text: String = ""
     @Published var showingSheet = false
-    @Published var savedSettings: GithubAppSaveModel
     
     let persistence: UserDefaultsManager = UserDefaultsManager()
     
-    init() {
-        savedSettings = GithubAppSaveModel(
-            isUsersChecked: false,
-            isRepositoriesChecked: true
-        )
-    }
-    
     @ViewBuilder
     func searchResultViewBuilder(text: String) -> some View {
-        SearchResultsView(
-            viewModel: SearchResultsViewModel(
-                usersSelected: savedSettings.isUsersChecked,
-                repositoriesSelected: savedSettings.isRepositoriesChecked,
-                githubRepository: GithubRepositoryImpl(),
-                userRepository: UsersRepositoryImpl(),
-                searchQuery: text
+        let saveModel = persistence.getGithubSettings()
+        switch saveModel {
+        case .success(let currentSaveModel):
+            SearchResultsView(
+                viewModel: SearchResultsViewModel(
+                    usersSelected: currentSaveModel.isUsersChecked,
+                    repositoriesSelected: currentSaveModel.isRepositoriesChecked,
+                    githubRepository: GithubRepositoryImpl(),
+                    userRepository: UsersRepositoryImpl(),
+                    searchQuery: text
+                )
             )
-        )
-    }
-    
-    func onAppear() {
-        do {
-            try savedSettings = persistence.getGithubSettings().get()
+        case .failure(let error):
+            ErrorView(error: error)
         }
-        catch {}
+        
     }
     
-    //MARK: Delegate Implementation
     func queryIsEmpty(text: String) -> Bool {
         return text.isEmpty
     }
-    
-    func getRepositories() -> Bool {
-        return savedSettings.isRepositoriesChecked
-    }
-    
-    func getUsers() -> Bool {
-        return savedSettings.isUsersChecked
-    }
-    
-    func toggleRepositories() {
-        savedSettings.isRepositoriesChecked.toggle()
-        persistence.saveGithubSettings(saveModel: savedSettings)
-    }
-    
-    func toggleUsers() {
-        savedSettings.isUsersChecked.toggle()
-        persistence.saveGithubSettings(saveModel: savedSettings)
-    }
-}
-
-//MARK: Delegate
-protocol HomeScreenViewModelDelegate {
-    func getRepositories() -> Bool
-    func getUsers() -> Bool
-    func toggleRepositories()
-    func toggleUsers()
 }
